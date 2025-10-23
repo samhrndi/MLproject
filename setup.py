a482918662cda1836e28b7a14a7dc4a56e485d38
Hessam Harandi <hessamharandi@Hessams-MacBook-Air.local>
2025-10-23 14:07:25 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> list[str]:
+    """This function will return the list of requirements"""
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+setup(
+    name="ml_project",
+    version="0.1.0",
+    author="Hessam",
+    author_email="SAM.HRNDI@GMAIL.COM",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
